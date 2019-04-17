e3c72c9998730221171f04a6bb0f1bf172a678bd
Gutnar Leede <gutnar@gmail.com>
2019-04-17 11:37:29 +0300

    Add calculating relative results

--- src/results.py
+++ src/results.py
@@ -1,4 +1,5 @@
 #%%
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -7,29 +8,51 @@ plt.rcParams["figure.figsize"] = [16, 10]
 plt.rcParams["font.size"] = 16
 
 #%%
-random_results = pd.read_csv("data/final/filament_galaxies_random.csv")
+results = {
+    "random": pd.read_csv("data/final/filament_galaxies_random.csv"),
+    "global": pd.read_csv("data/final/filament_galaxies_global.csv"),
+    "classifier": pd.read_csv("data/final/filament_galaxies_classifier.csv")
+}
 
-for method in ("random", "global", "classifier"):
-    results = pd.read_csv("data/final/filament_galaxies_%s.csv" % method)
-    
+for method in results:
     plt.figure(1)
     plt.plot(
-        results["max"],
-        results["N"],
+        results[method]["max"],
+        results[method]["N"],
         label=method
     )
 
     plt.figure(2)
     plt.plot(
-        results["max"],
-        (results["N"] - random_results["N"]) / random_results["N"],
+        results[method]["max"],
+        (results[method]["N"] - results["random"]["N"]) / results["random"]["N"],
         label=method
     )
 
+    if method != "random":
+        plt.figure(3)
+        plt.plot(
+            results[method]["max"],
+            (results[method]["N"] - results["global"]["N"]) / results["global"]["N"],
+            label=method
+        )
+
 plt.figure(1)
+plt.title("DUM")
 plt.gca().legend()
 plt.savefig("plots/dum_absolute.png")
 
 plt.figure(2)
+plt.title("DUM relative to random results")
 plt.gca().legend()
-plt.savefig("plots/dum_relative.png")
+plt.savefig("plots/dum_relative_to_random.png")
+
+plt.figure(3)
+plt.title("DUM relative to global method results")
+plt.gca().legend()
+plt.savefig("plots/dum_relative_to_global.png")
+
+#%%
+rel = (results["classifier"]["N"] - results["global"]["N"]) / results["global"]["N"]
+
+np.trapz(rel[:50]), np.trapz(rel[50:])
