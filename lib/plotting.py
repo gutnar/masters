Add correct triaxial galaxy spin vector calculation

lib/plotting.py
@@ -41,8 +41,8 @@ TP_GRID = np.column_stack((
 ))
 
 POS_INC_MESH = np.meshgrid(
-    np.linspace(-np.pi/2, np.pi/2, 100),
-    np.linspace(0, 1, 100)
+    np.linspace(0, np.pi/2, 100),
+    np.linspace(0, 1, 100),
 )
 POS_INC_GRID = np.column_stack((
     POS_INC_MESH[0].reshape(-1, 1),
@@ -60,14 +60,14 @@ def get_pdf(kde, grid, resample=False, N=10000):
     return kde(grid.T).reshape(100, 100)
 
 
-def plot_ba_results(ba, size=150000):
-    q, x, z, p, t = ba.sample(size)
+def plot_ba_2d_results(ba, size=150000):
+    q, xi, zeta, theta, phi = ba.sample(size)
     
     plt.xlim((0, 1))
     plt.plot(ba.q_pdf.x, ba.q_pdf.y, label="target")
     plt.hist(q, 100, (0, 1), True, label=r"$q$", histtype="step")
-    plt.hist(np.cos(t), 100, (0, 1), True, label=r"$\cos(\theta)$", histtype="step")
-    plt.hist(p/np.pi, 100, (0, 1), True, label=r"$\phi/\pi$", histtype="step")
+    plt.hist(np.cos(theta), 100, (0, 1), True, label=r"$\cos(\theta)$", histtype="step")
+    plt.hist(phi/np.pi, 100, (0, 1), True, label=r"$\phi/\pi$", histtype="step")
     plt.gca().legend()
 
 
@@ -102,22 +102,6 @@ def plot_xz_kde(ba, resample=True, **kwargs):
     plot_kde(ba.xz_kde, XZ_GRID, resample, aspect=1/2, **kwargs)
 
 
-def plot_qt_kde(ba, resample=True, **kwargs):
-    plt.xlabel(r"$q$")
-    plt.xticks(
-        [0, 19, 39, 59, 79, 99],
-        ["0.0", "0.2", "0.4", "0.6", "0.8", "1.0"]
-    )
-
-    plt.ylabel(r"$\theta$", rotation=0)
-    plt.yticks(
-        [0, 19, 39, 59, 79, 99],
-        [(r"$%d^∘$" % t) for t in np.linspace(-90, 90, 6)]
-    )
-
-    plot_kde(ba.qt_kde, QT_GRID, resample, aspect=1, **kwargs)
-
-
 def plot_qi_kde(ba, resample=True, **kwargs):
     plt.xlabel(r"$q$")
     plt.xticks(
@@ -132,22 +116,6 @@ def plot_qi_kde(ba, resample=True, **kwargs):
     )
 
     plot_kde(ba.qi_kde, QI_GRID, resample, aspect=1, **kwargs)
-
-
-def plot_qp_kde(ba, resample=True, **kwargs):
-    plt.xlabel(r"$q$")
-    plt.xticks(
-        [0, 19, 39, 59, 79, 99],
-        ["0.0", "0.2", "0.4", "0.6", "0.8", "1.0"]
-    )
-
-    plt.ylabel(r"$\phi$", rotation=0)
-    plt.yticks(
-        [0, 19, 39, 59, 79, 99],
-        [(r"$%d^∘$" % t) for t in np.linspace(-90, 90, 6)]
-    )
-
-    plot_kde(ba.qp_kde, QP_GRID, resample, aspect=1, **kwargs)
 
 
 def plot_tp_kde(ba, q, **kwargs):
@@ -171,24 +139,69 @@ def plot_tp_kde(ba, q, **kwargs):
 
     plot_kde(kde, TP_GRID, False, **kwargs)
 
-
-def plot_pos_inc_kde(ba, proj_q, proj_pos, **kwargs):
-    pos, inc = ba.sample_pos_inc(proj_q, proj_pos, 10000)
-
-    kde = stats.kde.gaussian_kde(
-        np.column_stack((pos, inc)).T, "scott"
+def plot_q_theta_kde(ba, **kwargs):
+    plt.xlabel(r"$q$")
+    plt.xticks(
+        [0, 19, 39, 59, 79, 99],
+        ["0.0", "0.2", "0.4", "0.6", "0.8", "1.0"]
     )
 
-    plt.xlabel("pos")
-    plt.xticks(
+    plt.ylabel(r"$\theta$", rotation=0)
+    plt.yticks(
         [0, 19, 39, 59, 79, 99],
         [(r"$%d^∘$" % t) for t in np.linspace(-90, 90, 6)]
     )
 
-    plt.ylabel("inc", rotation=0)
+    q, xi, zeta, theta, phi = ba.sample(10000)
+    kde = stats.kde.gaussian_kde(
+        np.column_stack((q, theta)).T
+    )
+
+    plot_kde(kde, QT_GRID, False, aspect=1, **kwargs)
+
+
+def plot_q_phi_kde(ba, **kwargs):
+    plt.xlabel(r"$q$")
+    plt.xticks(
+        [0, 19, 39, 59, 79, 99],
+        ["0.0", "0.2", "0.4", "0.6", "0.8", "1.0"]
+    )
+
+    plt.ylabel(r"$\phi$", rotation=0)
     plt.yticks(
         [0, 19, 39, 59, 79, 99],
-        [(r"$%.2f$" % t) for t in np.linspace(0, 1, 6)]
+        [(r"$%d^∘$" % t) for t in np.linspace(-90, 90, 6)]
+    )
+
+    q, xi, zeta, theta, phi = ba.sample(10000)
+    kde = stats.kde.gaussian_kde(
+        np.column_stack((q, phi)).T
+    )
+
+    plot_kde(kde, QP_GRID, False, aspect=1, **kwargs)
+
+
+def plot_pos_inc_kde(ba, q, p, **kwargs):
+    plt.xlabel("p")
+    plt.xticks(
+        [0, 19, 39, 59, 79, 99],
+        [(r"$%d^∘$" % t) for t in np.linspace(0, 90, 6)]
+    )
+
+    plt.ylabel(r"$cos(i)$", rotation=0)
+    plt.yticks(
+        [0, 19, 39, 59, 79, 99],
+        ["0.0", "0.2", "0.4", "0.6", "0.8", "1.0"]
+    )
+
+    p, i = ba.sample_pos_inc(q, p, 10000)
+    #spin_vec = ba.sample_spin_vec(q, p, 10000)
+    kde = stats.kde.gaussian_kde(
+        np.column_stack((
+            p, i
+            #abs(np.arctan(spin_vec[:,1] / spin_vec[:,0])),
+            #spin_vec[:,2]
+        )).T
     )
 
     plot_kde(kde, POS_INC_GRID, False, **kwargs)

src/main.py
@@ -22,15 +22,25 @@ sern_bins = np.linspace(0, max_sern, 51)
 dum_bins = np.linspace(0, 1, 101)
 
 #%%
-if sys.argv[3] == "global":
-    approximator = SampleApproximator(pd.read_csv("data/raw/data_gama_gal_orient.txt", r"\s+"))
-elif sys.argv[3] == "global1d":
+method = sys.argv[2]
+
+if method == "global":
+    method += "_" + sys.argv[3]
+    approximator = SampleApproximator(
+        pd.read_csv("data/raw/data_gama_gal_orient.txt", r"\s+"),
+        float(sys.argv[3])
+    )
+elif method == "global1d":
     approximator = SampleApproximator1d(pd.read_csv("data/raw/data_gama_gal_orient.txt", r"\s+"))
-elif sys.argv[3] == "classifier":
-    approximator = ClassifierApproximator(pd.read_csv("data/intermediate/train_galaxies.csv"))
-elif sys.argv[3] == "classifier1d":
+elif method == "classifier":
+    method += "_" + sys.argv[3]
+    approximator = ClassifierApproximator(
+        pd.read_csv("data/intermediate/train_galaxies.csv"),
+        float(sys.argv[3])
+    )
+elif method == "classifier1d":
     approximator = ClassifierApproximator1d(pd.read_csv("data/intermediate/train_galaxies.csv"))
-elif sys.argv[3] == "random":
+elif method == "random":
     approximator = RandomApproximator()
 
 #%%
@@ -60,7 +70,7 @@ def process_galaxies(galaxies):
 
 #%%
 if __name__ == "__main__":
-    galaxies = pd.read_csv("data/intermediate/%s.csv" % sys.argv[2])
+    galaxies = pd.read_csv("data/intermediate/filament_galaxies.csv")
     #galaxies = galaxies[:4]
 
     pool = Pool(processes)
@@ -79,5 +89,5 @@ if __name__ == "__main__":
     #})
     results = pd.DataFrame(hist)
 
-    results.to_csv("data/final/%s_%s.csv" % (sys.argv[2], sys.argv[3]), index=False)
+    results.to_csv("data/final/%s.csv" % method, index=False)
     #results.to_csv("data/final/test.csv", index=False)

src/results_hist.py
@@ -3,56 +3,41 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from lib import get_dum
-
 #%%
-hist_random = pd.read_csv("data/final/filament_galaxies_random.csv")
-hist_global = pd.read_csv("data/final/filament_galaxies_global.csv")
-hist_classifier = pd.read_csv("data/final/filament_galaxies_classifier.csv")
+results = {
+    "random": "data/final/filament_galaxies_random.csv",
+    "ba": "data/final/filament_galaxies_ba.csv",
+    "global1d": "data/final/filament_galaxies_global1d.csv",
+    "classifier1d": "data/final/filament_galaxies_classifier1d.csv",
+    "global": "data/final/global_0.csv"
+}
 
-plt.figure(1)
-plt.imshow(hist_random.values.T, origin="lower")
-
-plt.figure(2)
-plt.imshow(hist_global.values.T, origin="lower")
-
-plt.figure(3)
-plt.imshow(hist_classifier.values.T, origin="lower")
-
-#%%
 dum_values = (np.linspace(0, 1, 101)[:-1] + np.linspace(0, 1, 101)[1:]) / 2
 sern_values = (np.linspace(0, 20, 51)[:-1] + np.linspace(0, 20, 51)[1:]) / 2
 sern_bins = [0, 2, 20]
 
-hr = np.zeros((len(sern_bins) - 1, 100))
-hg = np.zeros((len(sern_bins) - 1, 100))
-hc = np.zeros((len(sern_bins) - 1, 100))
-
-for i in range(len(sern_bins) - 1):
-    hr[i] = hist_random[(sern_values >= sern_bins[i]) & (sern_values < sern_bins[i + 1])].sum(axis=0)
-    hg[i] = hist_global[(sern_values >= sern_bins[i]) & (sern_values < sern_bins[i + 1])].sum(axis=0)
-    hc[i] = hist_classifier[(sern_values >= sern_bins[i]) & (sern_values < sern_bins[i + 1])].sum(axis=0)
-
-plt.plot(sern_bins[1:], np.sum(hr * dum_values, axis=1) / np.sum(hr, axis=1), label="random")
-plt.plot(sern_bins[1:], np.sum(hg * dum_values, axis=1) / np.sum(hg, axis=1), label="global")
-plt.plot(sern_bins[1:], np.sum(hc * dum_values, axis=1) / np.sum(hc, axis=1), label="classifier")
-plt.legend()
-
 #%%
-for i in range(len(sern_bins) - 1):
-    plt.figure(i)
-    plt.title("%d < sern < %d" % (sern_bins[i], sern_bins[i+1]))
-    s = hr[i].sum()
-    plt.plot(dum_values, hr[i] / s, label="random")
-    plt.plot(dum_values, hg[i] / s, label="global")
-    plt.plot(dum_values, hc[i] / s, label="classifier")
-    plt.gca().legend()
+for index, (method, filename) in enumerate(results.items()):
+    hist = pd.read_csv(filename)
 
-"""
-plt.figure(len(sern_bins))
-plt.title("0 < sern < 20")
-plt.plot(dum_values, hist_random.sum(axis=0), label="random")
-plt.plot(dum_values, hist_global.sum(axis=0), label="global")
-plt.plot(dum_values, hist_classifier.sum(axis=0), label="classifier")
+    h = np.zeros((len(sern_bins) - 1, 100))
+    for i in range(len(sern_bins) - 1):
+        h[i] = hist[(sern_values >= sern_bins[i]) & (sern_values < sern_bins[i + 1])].sum(axis=0)
+    
+    plt.figure(1)
+    plt.plot(sern_bins[1:], np.sum(h * dum_values, axis=1) / np.sum(h, axis=1), label=method)
+
+    for i in range(len(sern_bins) - 1):
+        plt.figure(2 + i)
+        plt.title("%d < sern < %d" % (sern_bins[i], sern_bins[i+1]))
+        plt.plot(dum_values, h[i] / h[i].sum(), label=method)
+
+    #plt.figure(len(sern_bins) + 1 + index)
+    #plt.title(method)
+    #plt.imshow(hist.values.T, origin="lower")
+
+plt.figure(1)
 plt.gca().legend()
-"""
+
+plt.figure(2)
+plt.gca().legend()

src/test.py
@@ -118,10 +118,10 @@ plt.hist(values, 100, density=True, histtype="step")
 import scipy.stats as stats
 
 kde = stats.kde.gaussian_kde(np.column_stack((
-    np.random.normal(0.2, 0.1, 100000),
-    np.random.normal(0.85, 0.1, 100000),
-    np.random.uniform(0, 1, 100000),
-    np.random.uniform(0, 1, 100000)
+    np.random.normal(0.2, 0.1, 1000000),
+    np.random.normal(0.85, 0.1, 1000000),
+    np.random.uniform(0, 1, 1000000),
+    np.random.uniform(0, 1, 1000000)
 )).T)
 
-kde.resample(10000).T
+kde.resample(1000000).T

src/test_projection.py
@@ -1,11 +1,8 @@
+
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import pi, trigsimp, Symbol, symbols, sqrt, solve, Eq, cos, sin, tan, lambdify, Abs, atan, re
-from sympy.matrices import Matrix
-from sympy.vector import Vector
 from sympy import init_printing
-from sympy.abc import xi, zeta, phi, theta, psi, omega
 
 from lib.projection import *
 import analytical
@@ -13,12 +10,6 @@ import analytical
 #init_printing()
 
 #%%
-blender_phi = np.array((0, 15, 30, 45, 60, 75, 90)) / 180 * np.pi
-#blender_q = (0.9, 1.73/2, 1.57/2, 1.28/2, 0.92/2, 0.5/2, 0.1) # (0.1, 0.9, np.pi/2)
-blender_q = (1.41/1.79, 1.36/1.81, 1.2/1.84, 0.98/1.87, 0.7/1.88, 0.4/1.89, 0.2/1.9) # (0.1, 0.9, np.pi/4)
-blender_omega = np.array((0, -25, -45, -60, -70, -80, -90)) / 180 * np.pi + np.pi/2 # (0.1, 0.9, np.pi/4)
-
-plt.figure(1)
 plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = \pi / 4$")
 
 plt.plot(
@@ -37,148 +28,137 @@ plt.plot(
     analytical.get_q(
         0.1,
         0.9,
-        np.pi/4,
         np.linspace(0, 2*np.pi, 100),
-    ),
-    label="Binney"
-)
-
-plt.plot(blender_phi, blender_q, "o", label="Blender")
-plt.gca().legend()
-
-plt.figure(2)
-plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = \pi / 4$")
-
-plt.plot(
-    np.linspace(0, 2*np.pi, 100),
-    get_omega(
-        0.1,
-        0.9,
         np.pi/4,
-        np.linspace(0, 2*np.pi, 100)
-    ),
-    label=r"$\omega$"
-)
-
-plt.plot(blender_phi, blender_omega, "o", label="Blender")
-plt.gca().legend()
-
-plt.figure(3)
-plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = \pi / 2$")
-
-plt.plot(
-    np.linspace(0, 2*np.pi, 100),
-    get_q(
-        0.1,
-        0.9,
-        np.pi/2,
-        np.linspace(0, 2*np.pi, 100)
-    ),
-    label="q"
-)
-
-plt.plot(
-    np.linspace(0, 2*np.pi, 100),
-    analytical.get_q(
-        0.1,
-        0.9,
-        np.pi/2,
-        np.linspace(0, 2*np.pi, 100),
     ),
     "o",
-    markersize=3,
+    markersize=2,
     label="Binney"
 )
 
-plt.gca().legend()
-
-#%%
-plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = \pi / 3$")
-
-plt.plot(
-    np.linspace(0, 2*np.pi, 100),
-    get_q(
-        0.1,
-        0.9,
-        np.pi/3,
-        np.linspace(0, 2*np.pi, 100)
-    ),
-    label="q"
-)
-
-plt.plot(
-    np.linspace(0, 2*np.pi, 100),
-    analytical.get_q(
-        0.1,
-        0.9,
-        np.pi/3,
-        np.linspace(0, 2*np.pi, 100),
-    ),
-    label="Binney"
-)
-
-plt.gca().legend()
+plt.legend()
 
 #%%
 plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = 0$")
 
 plt.plot(
     np.linspace(0, 2*np.pi, 100),
-    get_q(
+    get_psi(
         0.1,
         0.9,
         0,
         np.linspace(0, 2*np.pi, 100)
     ),
-    label="q"
+    label=r"$\psi$"
 )
 
 plt.plot(
     np.linspace(0, 2*np.pi, 100),
-    analytical.get_q(
+    analytical.get_psi(
         0.1,
         0.9,
-        0,
         np.linspace(0, 2*np.pi, 100),
+        0,
     ),
+    "o",
+    markersize=2,
     label="Binney"
 )
 
-plt.gca().legend()
+plt.legend()
 
 #%%
-get_x_prime_vec = lambdify((theta, phi, psi), P.T * R.T * Matrix(3, 1, (1, 0, 0)))
+plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = \pi / 4$")
 
-def get_spin_vec(xi_value, zeta_value, theta_value, phi_value, p_value):
-    omega_value = get_omega(xi_value, zeta_value, theta_value, phi_value)
-    
-    return get_x_prime_vec(
-        theta_value,
-        phi_value,
-        p_value - (omega_value + np.pi/2)
-    ).T
+plt.plot(
+    np.linspace(0, 2*np.pi, 100),
+    get_psi(
+        0.1,
+        0.9,
+        np.pi/4,
+        np.linspace(0, 2*np.pi, 100)
+    ),
+    label=r"$\psi$"
+)
 
-i = 0.2
-p = 0.5
+plt.plot(
+    np.linspace(0, 2*np.pi, 100),
+    analytical.get_psi(
+        0.1,
+        0.9,
+        np.linspace(0, 2*np.pi, 100),
+        np.pi/4,
+    ),
+    "o",
+    markersize=2,
+    label="Binney"
+)
+
+plt.legend()
+
+#%%
+plt.title(r"$\xi = 0.1, \zeta=0.9, \theta = \pi / 2$")
+
+plt.plot(
+    np.linspace(0, 2*np.pi, 100),
+    get_psi(
+        0.5,
+        0.9,
+        np.pi/5*8,
+        np.linspace(0, 2*np.pi, 100)
+    ),
+    label=r"$\psi$"
+)
+
+plt.plot(
+    np.linspace(0, 2*np.pi, 100),
+    analytical.get_psi(
+        0.5,
+        0.9,
+        np.linspace(0, 2*np.pi, 100),
+        np.pi/5*8,
+    ),
+    "o",
+    markersize=2,
+    label="Binney"
+)
+
+plt.legend()
+
+#%%
+i = 0.6
+p = 0.8
 
 u = -np.sin(i) * np.cos(p)
 v = np.sin(i) * np.sin(p)
 w = np.cos(i)
 
-get_spin_vec(
+get_z_prime_vec(
     0.2,
     1,
-    np.pi/2,
     i,
+    0,
     p
-), np.array([u, v, w])
+).T[0], np.array([u, v, w])
 
 #%%
-P.T * R.T * Matrix(3, 1, (1, 0, 0))
-
-#%%
-from sympy import pi
-
-(P.T * R.T * Matrix(3, 1, (1, 0, 0))).subs({
-    theta: pi/2
+z_prime_vec.subs({
+    xi: 0.1,
+    zeta: 0.9,
+    theta: 0.123,
+    phi: 0.666,
+    p: 0
 })
+
+#%%
+np.cos(i)
+
+#%%
+R.T * Matrix(3, 1, (1, 0, 0))
+
+#%%
+simplify(z_prime_vec.subs({
+    zeta: 1,
+    psi: 0,
+    phi: 0
+}))

src/tp_methods.py
@@ -4,13 +4,14 @@ from lib import *
 
 
 class SampleApproximator:
-    def __init__(self, galaxies):
+    def __init__(self, galaxies, z_min=0):
         q_pdf = PDF.from_samples(
             np.linspace(0, 1, 100),
             galaxies["ba"].values
         )
 
-        self.ba = BayesianApproximation(q_pdf)
+        self.ba = BayesianApproximation2d(q_pdf)
+        self.ba.Z_MIN = z_min
         self.ba.run()
     
     def sample_pos_inc(self, galaxy, N):
@@ -39,14 +40,16 @@ class SampleApproximator1d:
 
 
 class ClassifierApproximator:
-    def __init__(self, galaxies, **kwargs):
+    def __init__(self, galaxies, z_min=0, **kwargs):
+        self.z_min = z_min
         self.classifier = Classifier(**kwargs)
         self.classifier.fit(galaxies)
 
     def sample_pos_inc(self, galaxy, N):
         q_pdf = self.classifier.predict_pdf(galaxy)
 
-        ba = BayesianApproximation(q_pdf)
+        ba = BayesianApproximation2d(q_pdf)
+        ba.Z_MIN = self.z_min
         ba.run()
 
         return ba.sample_pos_inc(