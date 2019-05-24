f67d910b8700f429641ca56484cc3fa1dd42f902
Gutnar Leede <gutnar@gmail.com>
2019-05-24 10:38:47 +0300

    Update galaxy classes list

--- src/common.py
+++ src/common.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-n_clusters = 4
+n_clusters = 8
 
 galaxy_classes = (
     { "label": "e_spiral", "parameter": "e_class", "value": 0 },
@@ -8,5 +8,9 @@ galaxy_classes = (
     { "label": "g_spiral", "parameter": "g_class", "value": 0 },
     { "label": "g_elliptic", "parameter": "g_class", "value": 1 },
     { "label": "g_unknown1", "parameter": "g_class", "value": 2 },
-    { "label": "g_unknown2", "parameter": "g_class", "value": 3 }
+    { "label": "g_unknown2", "parameter": "g_class", "value": 3 },
+    { "label": "g_unknown3", "parameter": "g_class", "value": 4 },
+    { "label": "g_unknown4", "parameter": "g_class", "value": 5 },
+    { "label": "g_unknown5", "parameter": "g_class", "value": 6 },
+    { "label": "g_unknown6", "parameter": "g_class", "value": 7 }
 )
